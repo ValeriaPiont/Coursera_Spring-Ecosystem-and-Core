ffa556dedb922e11d4c8a7a22b283be5ed826f24
Valeria Piont <iceflame.vvv@gmail.com>
2022-05-09 12:40:09 +0300

    lab4.2: created annotation configuration with javax

--- src/main/java/com/coursera/spring_ecosystem_and_core/service/StudentServiceImpl.java
+++ src/main/java/com/coursera/spring_ecosystem_and_core/service/StudentServiceImpl.java
@@ -3,26 +3,22 @@ package com.coursera.spring_ecosystem_and_core.service;
 import com.coursera.spring_ecosystem_and_core.core.Student;
 import com.coursera.spring_ecosystem_and_core.dao.StudentDao;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
+import javax.inject.Named;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
 @Setter
-@Component(value = "studentService")
+@Named(value = "studentService")
 public class StudentServiceImpl implements StudentService {
 
+    @Resource(name = "studentDaoImpl")
     private StudentDao studentDao;
 
     @Value("2")
     private int numberOfStudents;
-
-    @Autowired
-    public StudentServiceImpl(StudentDao studentDao) {
-        this.studentDao = studentDao;
-    }
 
     @Override
     public Student get(long id) {
